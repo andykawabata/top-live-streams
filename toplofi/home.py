380301200f8e855d89bf190814b25b0f430fc368
Andy Kawabata <andykawabata@gmail.com>
2019-08-12 11:04:27 -0400

    results=20

--- toplofi/home.py
+++ toplofi/home.py
@@ -4,5 +4,5 @@ from flask import render_template
 
 @app.route("/")
 def home():
-    streams = getStreams(maxResults=25)
+    streams = getStreams(maxResults=20)
     return render_template("home.html", streams=streams)
