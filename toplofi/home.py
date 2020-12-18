410390d6b4b712d8a42b73d853a080a8646261be
Andy Kawabata <andykawabata@gmail.com>
2020-12-18 11:44:35 -0500

    m

--- toplofi/home.py
+++ toplofi/home.py
@@ -1,16 +1,16 @@
-#from toplofi import app
-from toplofi.main import *
-from flask import render_template
-from flask import Flask
-from config import Config
-
-app = Flask(__name__)
-app.config.from_object(Config)
-
-@app.route("/")
-def home():
-    streams = getStreams(maxResults=20)
-    return render_template("home.html", streams=streams)
+# #from toplofi import app
+# from toplofi.main import *
+# from flask import render_template
+# from flask import Flask
+# from config import Config
+#
+# app = Flask(__name__)
+# app.config.from_object(Config)
+#
+# @app.route("/")
+# def home():
+#     streams = getStreams(maxResults=20)
+#     return render_template("home.html", streams=streams)
 
 
 if __name__ == "__main__":
